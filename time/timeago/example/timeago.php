c2d48b84723407a9695f38ae63985cc35726f9cf
IsmailYunluTn <88527782+IsmailYunluTn@users.noreply.github.com>
2022-05-19 20:03:35 +0300

    Delete time/timeago directory

--- time/timeago/example/timeago.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require_once ("funcs/timeago.php");
-    $timeago = new timeAgoClass("Yıl", "Ay", "Gün", "Saat", "Dakika", "Saniye"); // Yıl, Ay, Gün, Saat, Dakika, Saniye isimlerini belirliyoruz.
-    var_dump($timeago->timeAgo("2022-05-18", "2022-05-19")); // 2 tane tarih arasında geçen süre hesaplanıyor.
-    var_dump($timeago->timeAgo("2022-05-18")); // Şuanki tarih ile geçen tarih süresi hesaplanıyor.
-?>
